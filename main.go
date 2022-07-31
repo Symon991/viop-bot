329d3a2ced06ea4b168c83e27a4155257b2b04fa
Symon991 <symon991@gmail.com>
2022-07-31 19:27:55 +0200

    feat: better error handling

--- main.go
+++ main.go
@@ -9,11 +9,47 @@ import (
 	"log"
 	"net/url"
 	"os"
+
+	"golang.org/x/net/websocket"
 )
 
 func main() {
 
-	log.Print("START")
+	discordErrorChan := make(chan error)
+	twitterErrorChan := make(chan error)
+
+	conn := startDiscord(discordErrorChan)
+	defer conn.Close()
+	startTwitter(twitterErrorChan)
+
+	for {
+		select {
+		case err := <-discordErrorChan:
+			log.Print(err)
+			conn.Close()
+			conn = startDiscord(discordErrorChan)
+
+		case err := <-twitterErrorChan:
+			log.Print(err)
+			startTwitter(twitterErrorChan)
+		}
+	}
+}
+
+func startDiscord(errorChan chan error) *websocket.Conn {
+
+	conn, interval := discord.Connect()
+	discord.Heartbeat(interval, conn, errorChan)
+	discord.Identify(conn, os.Getenv("DISCORD_APPLICATION_ID"))
+	go discord.Listen(conn, commands.HandleInteraction, errorChan)
+}
+
+func startTwitter(errorChan chan error) {
+
+	go twitter.Monitor(errorChan)
+}
+
+func startRedis() {
 
 	redisCloudUrlEnv := os.Getenv("REDISCLOUD_URL")
 	redisCloudUrl, err := url.Parse(redisCloudUrlEnv)
@@ -21,31 +57,4 @@ func main() {
 		log.Panic(fmt.Errorf("paring redis cloud url: %w", err))
 	}
 	cache.Connect(redisCloudUrl)
-
-	discordErrorChan := make(chan error)
-	twitterErrorChan := make(chan error)
-
-	conn, interval := discord.Connect()
-	discord.Heartbeat(interval, conn, discordErrorChan)
-	discord.Identify(conn, os.Getenv("DISCORD_APPLICATION_ID"))
-	go discord.Listen(conn, commands.HandleInteraction, discordErrorChan)
-
-	go twitter.Monitor(twitterErrorChan)
-
-	for {
-		select {
-		case <-discordErrorChan:
-			log.Print(err)
-
-			conn.Close()
-
-			conn, interval := discord.Connect()
-			discord.Heartbeat(interval, conn, discordErrorChan)
-			discord.Identify(conn, os.Getenv("DISCORD_APPLICATION_ID"))
-			go discord.Listen(conn, commands.HandleInteraction, discordErrorChan)
-
-		case <-twitterErrorChan:
-			go twitter.Monitor(twitterErrorChan)
-		}
-	}
 }
--- twitter/monitor.go
+++ twitter/monitor.go
@@ -10,20 +10,29 @@ import (
 func Monitor(errorChan chan error) {
 
 	channel := make(chan StreamMessage, 1)
-	go Stream(channel, errorChan)
+
+	streamErrorChan := make(chan error)
+	go Stream(channel, streamErrorChan)
 
 	for {
-		tweet := (<-channel)
-		var matchingRules []string
-		for _, matchingRule := range tweet.MatchingRules {
-			matchingRules = append(matchingRules, matchingRule.Tag)
-		}
-		err := discord.PostChannelMessage(messages.ChannelMessage{
-			Content: fmt.Sprintf("[%s] https://twitter.com/user/status/%s", strings.Join(matchingRules, ", "), tweet.Data.ID),
-		})
-		if err != nil {
-			errorChan <- fmt.Errorf("post tweet on channel: %w", err)
-			return
+		select {
+		case err := <-streamErrorChan:
+			{
+				errorChan <- fmt.Errorf("twitter stream: %w", err)
+			}
+		default:
+			tweet := (<-channel)
+			var matchingRules []string
+			for _, matchingRule := range tweet.MatchingRules {
+				matchingRules = append(matchingRules, matchingRule.Tag)
+			}
+			err := discord.PostChannelMessage(messages.ChannelMessage{
+				Content: fmt.Sprintf("[%s] https://twitter.com/user/status/%s", strings.Join(matchingRules, ", "), tweet.Data.ID),
+			})
+			if err != nil {
+				errorChan <- fmt.Errorf("post tweet on channel: %w", err)
+				return
+			}
 		}
 	}
 }
